ca33dcc0298fa95a1b780a654f24f573321afd93
lijieniu <niulijie19890522@163.com>
2018-12-08 16:33:05 +0800

    add menu

--- app/web/framework/i18n/admin/cn.js
+++ app/web/framework/i18n/admin/cn.js
@@ -2,9 +2,17 @@
 export default {
   menu: {
     home: '首页',
+    attendancemanage: '考勤管理',
+    actualnumber: '实到人数',
+    absencenumber: '缺勤人数',
+    leavenumber: '请假人数',
+    summarymanage: '汇总管理',
+    daysummary: '日汇总',
+    weeksummary: '周汇总',
+    monthsummary: '月汇总',
     articlemanage: '文章管理',
     articlequery: '文章查询',
-    articleadd: '添加文章'
+    articleadd: '添加文章',
   },
   lang: {
     href: '/admin?locale=en',
