bcdc2007841150f8cb415536787781673330b848
leiphone5s <2449904029@qq.com>
2018-12-09 13:32:53 +0800

    添加汇总和考勤页面

--- app/web/page/admin/router/index.js
+++ app/web/page/admin/router/index.js
@@ -3,6 +3,12 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Dashboard from '../view/dashboard/index.vue';
 import ArticleList from '../view/article/list.vue';
+import AbsenseNumber from '../attendance/absencenumber.vue';
+import ActualNumber from '../attendance/actualnumber.vue';
+import LeaveNumber from '../attendance/leavenumber.vue';
+import DaySummary from '../summary/daysummary.vue';
+import MonthSummary from '../summary/monthsummary.vue';
+import WeekSummary from '../summary/weeksummary.vue';
 
 Vue.use(VueRouter);
 
@@ -27,6 +33,30 @@ export default function createRouter() {
         path: '/article/detail/:id',
         component: () => import('../view/article/detail.vue')
       },
+        {
+          path:'/attendance/absence',
+            component:AbsenseNumber
+        },
+        {
+          path:'/attendance/actual',
+            component:ActualNumber
+        },
+        {
+          path:'/attendance/leave',
+            component:LeaveNumber
+        },
+        {
+          path:'/summary/day',
+            component:DaySummary
+        },
+        {
+          path:'/summary/month',
+            component:MonthSummary
+        },
+        {
+          path:'/summary/week',
+            component:WeekSummary
+        },
       {
         path: '*', component: () => import('../view/notfound.vue')
       }
