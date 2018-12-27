f5b0c28199f397733530ae099de05aefb10025d3
lijieniu <niulijie19890522@163.com>
2018-12-27 10:47:13 +0800

    add mysql

--- app/router.js
+++ app/router.js
@@ -2,7 +2,8 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.admin.home);
-  router.post('/admin/api/article/list', controller.admin.list);
+  router.get('/admin/api/article/list', controller.admin.list);
+  router.get('/admin/api/user/list', controller.user.list);
   router.post('/admin/api/article/add', controller.admin.add);
   router.get('/admin/api/article/del/:id', controller.admin.del);
   router.get('/admin/api/article/:id', controller.admin.detail);
---
+++ app/service/user.js
@@ -0,0 +1,20 @@
+const Service = require('egg').Service;
+const Collection = require('../lib/db/collection');
+const Query = require('../lib/db/query');
+
+class UserService extends Service {
+    constructor(ctx) {
+        super(ctx);
+        this.ctx = ctx;
+        this.collection = new Collection(ctx.db, 'user');
+    }
+    async getUserList() {
+        const query = new Query();
+        query.pageIndex = 1;
+        query.pageSize = 10;
+        let userList = this.collection.getPager(query);
+        return userList;
+    }
+}
+
+module.exports = UserService;
